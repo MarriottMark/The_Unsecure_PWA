679160439849253cca9a2aa9baf510f9bfdc64c1
Mark M <135105725+MarriottMark@users.noreply.github.com>
2025-11-23 21:38:28 +0000

    A commit, maybe the first, I'm not to sure

--- user_management.py
+++ user_management.py
@@ -17,31 +17,47 @@ def insertUser(username, password, DoB):
 def retrieveUsers(username, password):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
-    cur.execute(f"SELECT * FROM users WHERE username = '{username}'")
+    # Original
+    # Original was f string that was implementing the User's input in the username field directly into the SQL query making it runnable.
+    # cur.execute(
+    #     "SELECT * FROM users WHERE username = ?",  # putting ? means using the placeholder fro the next parameter.
+    #     (username,),
+    # )  # The parameter on which ? works.
+
+    # if cur.fetchone() == None:
+    #     con.close()
+    #     return False
+    # else:
+    #     # This statement is really bad as it just looks for if there is a password that is entered, not necessarily matching the users input.
+    #     cur.execute("SELECT * FROM users WHERE password = ?", (password))
+    #     # Plain text log of visitor count as requested by Unsecure PWA management
+    #     with open("visitor_log.txt", "r") as file:
+    #         number = int(file.read().strip())
+    #         number += 1
+    #     with open("visitor_log.txt", "w") as file:
+    #         file.write(str(number))
+    #     # Simulate response time of heavy app for testing purposes
+    #     time.sleep(random.randint(80, 90) / 1000)
+    #     con.close()
+    #     return True
+
+    # New
+    cur.execute(
+        "SELECT * FROM users WHERE username == ? AND password == ?",
+        (username, password),
+    )
+
     if cur.fetchone() == None:
         con.close()
         return False
     else:
-        cur.execute(f"SELECT * FROM users WHERE password = '{password}'")
-        # Plain text log of visitor count as requested by Unsecure PWA management
-        with open("visitor_log.txt", "r") as file:
-            number = int(file.read().strip())
-            number += 1
-        with open("visitor_log.txt", "w") as file:
-            file.write(str(number))
-        # Simulate response time of heavy app for testing purposes
-        time.sleep(random.randint(80, 90) / 1000)
-        if cur.fetchone() == None:
-            con.close()
-            return False
-        else:
-            con.close()
-            return True
+        return True
 
 
 def insertFeedback(feedback):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
+    # Fix
     cur.execute(f"INSERT INTO feedback (feedback) VALUES ('{feedback}')")
     con.commit()
     con.close()
